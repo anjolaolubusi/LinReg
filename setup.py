578a0fcb28ae92810eabd3c9f6bd0a53bbc1c40d
Anjolaoluwa Olubusi <anjolaolubusi@gmail.com>
2019-06-30 11:47:49 -0400

    Re-arranging project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='linreg',
+      version='0.1',
+      description='A little linear regression package',
+      url='https://github.com/anjolaolubusi/LinReg',
+      author='Anjolaoluwa Olubusi',
+      author_email='anjolaolubusi@gmail.com',
+      license='MIT',
+      zip_safe=False)

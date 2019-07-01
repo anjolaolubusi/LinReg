47a774cebd9828e1e3f03a5286a877cdf642ebef
Anjolaoluwa Olubusi <anjolaolubusi@gmail.com>
2019-06-30 21:47:57 -0400

    E

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
-setuptools.setup(name='linreg-lib',
+setuptools.setup(name='linreglib',
       version='0.1',
       description='A little linear regression library package',
-      url='https://github.com/anjolaolubusi/linreg-lib',
+      url='https://github.com/anjolaolubusi/linreglib',
       author='Anjolaoluwa Olubusi',
       author_email='anjolaolubusi@gmail.com',
       license='MIT',
